efcbf4a8db4071b9ff8cac04585d51fdae628c15
Uzzal-Bhowmik <ubcreativity111@gmail.com>
2025-01-25 23:36:59 +0600

    jwt sign issue fixed

--- src/modules/auth/auth.utils.ts
+++ src/modules/auth/auth.utils.ts
@@ -1,13 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import jwt, { JwtPayload } from "jsonwebtoken";
 
 export const generateToken = (
   jwtPayload: JwtPayload,
   secret: string,
-  expiresIn: string,
+  expiresIn: any,
 ) => {
-  const token = jwt.sign(jwtPayload, secret, {
-    expiresIn,
-  });
+  const token = jwt.sign(
+    {
+      data: jwtPayload,
+    },
+    secret,
+    { expiresIn },
+  );
 
   return token;
 };
